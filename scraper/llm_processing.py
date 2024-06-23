1ffb61c0fa579c9cb886d8d9ecb8c2b15152b4b4
Tom <tomzarowland@gmail.com>
2024-06-23 16:08:28 +0100

    func to hit gpt 4o

---
+++ scraper/llm_processing.py
@@ -0,0 +1,29 @@
+import dotenv
+import json
+import os
+import requests
+
+dotenv.load_dotenv()
+
+
+def openai_completion(prompt, input: str) -> str:
+    url = "https://api.openai.com/v1/chat/completions"
+    headers = {
+        "Content-Type": "application/json",
+        "Authorization": "Bearer " + os.getenv("OPENAI_API_KEY"),
+    }
+
+    body = {
+        "model": "gpt-4o",
+        "messages": [
+            {"role": "system", "content": prompt},
+            {"role": "user", "content": input},
+        ],
+        "temperature": 0.5,
+    }
+    data = json.dumps(body)
+
+    response = requests.post(url, headers=headers, data=data).json()
+    output = response["choices"][0]["message"]["content"]
+
+    return output
---
+++ scraper/test/test_llm_processing.py
@@ -0,0 +1,11 @@
+from .. import llm_processing
+
+
+def test_openai_completion():
+    prompt = (
+        "Your job is to write a 3 sentence story about the topic provided by the user."
+    )
+    input = "Theo Walcott"
+
+    output = llm_processing.openai_completion(prompt, input)
+    print(output)
